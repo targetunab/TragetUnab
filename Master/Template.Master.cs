85e3c87820c15d993a97d1d46896fea29d26be84
targetunab <32179219+targetunab@users.noreply.github.com>
2017-09-21 19:28:51 -0400

    Master

---
+++ Master/Template.Master.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Target.Master
+{
+    public partial class Template : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void visibleBotonera(string html)
+        {
+            botonera.InnerHtml = html;
+        }
+    }
+}
